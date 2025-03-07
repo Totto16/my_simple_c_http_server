b952ed6447da29c5e4540ed4fcce190d3ddf6ea1
Totto16 <tobiausgais@gmail.com>
2025-03-08 00:51:14 +0100

    fix:make ftp commands case insensitive

--- src/ftp/command.c
+++ src/ftp/command.c
@@ -30,47 +30,47 @@ FTPCommand* parseSingleFTPCommand(char* commandStr) {
 	}
 
 	// see https://datatracker.ietf.org/doc/html/rfc959 5.3.1
-	if(strcmp("CDUP", commandStr) == 0) {
+	if(strcasecmp("CDUP", commandStr) == 0) {
 		command->type = FTP_COMMAND_CDUP;
 		return command;
-	} else if(strcmp("QUIT", commandStr) == 0) {
+	} else if(strcasecmp("QUIT", commandStr) == 0) {
 		command->type = FTP_COMMAND_QUIT;
 		return command;
-	} else if(strcmp("REIN", commandStr) == 0) {
+	} else if(strcasecmp("REIN", commandStr) == 0) {
 		command->type = FTP_COMMAND_REIN;
 		return command;
-	} else if(strcmp("PASV", commandStr) == 0) {
+	} else if(strcasecmp("PASV", commandStr) == 0) {
 		command->type = FTP_COMMAND_PASV;
 		return command;
-	} else if(strcmp("STOU", commandStr) == 0) {
+	} else if(strcasecmp("STOU", commandStr) == 0) {
 		command->type = FTP_COMMAND_STOU;
 		return command;
-	} else if(strcmp("ABOR", commandStr) == 0) {
+	} else if(strcasecmp("ABOR", commandStr) == 0) {
 		command->type = FTP_COMMAND_ABOR;
 		return command;
-	} else if(strcmp("PWD", commandStr) == 0) {
+	} else if(strcasecmp("PWD", commandStr) == 0) {
 		command->type = FTP_COMMAND_PWD;
 		return command;
-	} else if(strcmp("LIST", commandStr) == 0) {
+	} else if(strcasecmp("LIST", commandStr) == 0) {
 		command->type = FTP_COMMAND_LIST;
 		command->data.string = NULL; // signifies, that this has an optional argument
 		return command;
-	} else if(strcmp("NLST", commandStr) == 0) {
+	} else if(strcasecmp("NLST", commandStr) == 0) {
 		command->type = FTP_COMMAND_NLST;
 		command->data.string = NULL; // signifies, that this has an optional argument
 		return command;
-	} else if(strcmp("SYST", commandStr) == 0) {
+	} else if(strcasecmp("SYST", commandStr) == 0) {
 		command->type = FTP_COMMAND_SYST;
 		return command;
-	} else if(strcmp("STAT", commandStr) == 0) {
+	} else if(strcasecmp("STAT", commandStr) == 0) {
 		command->type = FTP_COMMAND_STAT;
 		command->data.string = NULL; // signifies, that this has an optional argument
 		return command;
-	} else if(strcmp("HELP", commandStr) == 0) {
+	} else if(strcasecmp("HELP", commandStr) == 0) {
 		command->type = FTP_COMMAND_HELP;
 		command->data.string = NULL; // signifies, that this has an optional argument
 		return command;
-	} else if(strcmp("NOOP", commandStr) == 0) {
+	} else if(strcasecmp("NOOP", commandStr) == 0) {
 		command->type = FTP_COMMAND_NOOP;
 		return command;
 	}
@@ -87,95 +87,95 @@ FTPCommand* parseSingleFTPCommand(char* commandStr) {
 
 	char* argumentStr = resultingIndex + 1;
 
-	if(strcmp("USER", commandStr) == 0) {
+	if(strcasecmp("USER", commandStr) == 0) {
 		command->type = FTP_COMMAND_USER;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("PASS", commandStr) == 0) {
+	} else if(strcasecmp("PASS", commandStr) == 0) {
 		command->type = FTP_COMMAND_PASS;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("ACCT", commandStr) == 0) {
+	} else if(strcasecmp("ACCT", commandStr) == 0) {
 		command->type = FTP_COMMAND_ACCT;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("CWD", commandStr) == 0) {
+	} else if(strcasecmp("CWD", commandStr) == 0) {
 		command->type = FTP_COMMAND_CWD;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("SMNT", commandStr) == 0) {
+	} else if(strcasecmp("SMNT", commandStr) == 0) {
 		command->type = FTP_COMMAND_SMNT;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("RETR", commandStr) == 0) {
+	} else if(strcasecmp("RETR", commandStr) == 0) {
 		command->type = FTP_COMMAND_RETR;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("STOR", commandStr) == 0) {
+	} else if(strcasecmp("STOR", commandStr) == 0) {
 		command->type = FTP_COMMAND_STOR;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("APPE", commandStr) == 0) {
+	} else if(strcasecmp("APPE", commandStr) == 0) {
 		command->type = FTP_COMMAND_APPE;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("RNFR", commandStr) == 0) {
+	} else if(strcasecmp("RNFR", commandStr) == 0) {
 		command->type = FTP_COMMAND_RNFR;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("RNTO", commandStr) == 0) {
+	} else if(strcasecmp("RNTO", commandStr) == 0) {
 		command->type = FTP_COMMAND_RNTO;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("DELE", commandStr) == 0) {
+	} else if(strcasecmp("DELE", commandStr) == 0) {
 		command->type = FTP_COMMAND_DELE;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("RMD", commandStr) == 0) {
+	} else if(strcasecmp("RMD", commandStr) == 0) {
 		command->type = FTP_COMMAND_RMD;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("MKD", commandStr) == 0) {
+	} else if(strcasecmp("MKD", commandStr) == 0) {
 		command->type = FTP_COMMAND_MKD;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("LIST", commandStr) == 0) {
+	} else if(strcasecmp("LIST", commandStr) == 0) {
 		command->type = FTP_COMMAND_LIST;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("NLST", commandStr) == 0) {
+	} else if(strcasecmp("NLST", commandStr) == 0) {
 		command->type = FTP_COMMAND_NLST;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("SITE", commandStr) == 0) {
+	} else if(strcasecmp("SITE", commandStr) == 0) {
 		command->type = FTP_COMMAND_SITE;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("STAT", commandStr) == 0) {
+	} else if(strcasecmp("STAT", commandStr) == 0) {
 		command->type = FTP_COMMAND_STAT;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("HELP", commandStr) == 0) {
+	} else if(strcasecmp("HELP", commandStr) == 0) {
 		command->type = FTP_COMMAND_HELP;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("AUTH", commandStr) == 0) {
+	} else if(strcasecmp("AUTH", commandStr) == 0) {
 		command->type = FTP_COMMAND_AUTH;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("ADAT", commandStr) == 0) {
+	} else if(strcasecmp("ADAT", commandStr) == 0) {
 		command->type = FTP_COMMAND_ADAT;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("MIC", commandStr) == 0) {
+	} else if(strcasecmp("MIC", commandStr) == 0) {
 		command->type = FTP_COMMAND_MIC;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("CONF", commandStr) == 0) {
+	} else if(strcasecmp("CONF", commandStr) == 0) {
 		command->type = FTP_COMMAND_CONF;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
-	} else if(strcmp("ENC", commandStr) == 0) {
+	} else if(strcasecmp("ENC", commandStr) == 0) {
 		command->type = FTP_COMMAND_ENC;
 		MAKE_STRING_ARG(argumentStr);
 		return command;
