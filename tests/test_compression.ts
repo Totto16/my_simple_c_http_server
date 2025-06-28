56a196fbf3da93e7868c1b9f6564eb2d6373da46
Totto16 <tobiausgais@gmail.com>
2025-06-28 21:30:46 +0200

    todo: add test file template for compression tests

---
+++ tests/test_compression.ts
@@ -0,0 +1,10 @@
+
+
+//TODO: test compression e.g.
+
+//
+
+
+//  curl -s -H 'Accept-Encoding: deflate' "http://localhost:8080/json" | openssl zlib -d
+
+// curl -s -H 'Accept-Encoding: gzip,deflate' "http://localhost:8080/json"  | gunzip
