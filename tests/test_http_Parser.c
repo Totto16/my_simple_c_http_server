016cb4053d127f3b28b26997d07b0f7591936179
Totto16 <tobiausgais@gmail.com>
2025-06-28 16:16:25 +0200

    todo: add potential test for compression parsing

---
+++ tests/test_http_Parser.c
@@ -0,0 +1,10 @@
+
+
+//TODO: test these cases
+
+
+"Accept-Encoding: compress, gzip",
+"Accept-Encoding:",
+"Accept-Encoding: *",
+"Accept-Encoding: compress;q=0.5, gzip;q=1.0",
+"Accept-Encoding: gzip;q=1.0, identity; q=0.5, *;q=0",
