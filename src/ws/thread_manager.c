ebc7555a862c0633ce6d00095b4cfafcf2043e18
Totto16 <tobiausgais@gmail.com>
2025-06-29 19:20:34 +0200

    feat: refactor random getter into utils file, so that it can be reused by other code

--- src/ws/thread_manager.c
+++ src/ws/thread_manager.c
@@ -14,7 +14,6 @@
 #include <pthread.h>
 
 #include <stdlib.h>
-#include <sys/random.h>
 #include <time.h>
 #include <utf8proc.h>
 
@@ -309,23 +308,8 @@ NODISCARD static int ws_send_message_raw_internal(WebSocketConnection* connectio
 	}
 
 	if(mask) {
-#ifdef __APPLE__
-		srandom(time(NULL));
-		uint32_t mask_byte = random();
-#else
-		uint32_t mask_byte = 0;
-		ssize_t result = getrandom((uint8_t*)(&mask_byte), sizeof(uint32_t), 0);
-		if(result != sizeof(uint32_t)) {
-			if(result < 0) {
-				LOG_MESSAGE(LogLevelWarn, "Get random failed: %s\n", strerror(errno));
-			}
 
-			unsigned int seed = time(NULL);
-
-			// use rand_r like normal rand:
-			mask_byte = rand_r(&seed);
-		}
-#endif
+		uint32_t mask_byte = get_random_bytes();
 
 		*((uint32_t*)(resultingFrame + RAW_MESSAGE_HEADER_SIZE + payload_additional_len)) =
 		    mask_byte;
