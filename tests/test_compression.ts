6751a555f0e4467788776823f3ef09330bc13e37
Totto16 <tobiausgais@gmail.com>
2025-06-29 19:50:21 +0200

    fix: add zstd example for cli usage

--- tests/test_compression.ts
+++ tests/test_compression.ts
@@ -7,3 +7,5 @@
 // curl -s -H 'Accept-Encoding: gzip' "http://localhost:8080/json" | gunzip
 
 // curl -s -H 'Accept-Encoding: br' "http://localhost:8080/json" | brotli -d --stdout
+
+// curl -s -H 'Accept-Encoding: zstd' "http://localhost:8080/json" | zstd -d
