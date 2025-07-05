ba04fbe165681855f228c5a59b97073774f1b7e5
Totto16 <tobiausgais@gmail.com>
2025-07-06 01:43:32 +0200

    feat: respond to the websocket extension request correctly

--- src/ws/ws.c
+++ src/ws/ws.c
@@ -301,6 +301,106 @@ static void parse_ws_extensions(WSExtensions* extensions, const char* const valu
 	free(value);
 }
 
+NODISCARD static bool append_ws_extension_as_string(StringBuilder* string_builder,
+                                                    WSExtension extension) {
+
+	switch(extension.type) {
+		case WSExtensionTypePerMessageDeflate: {
+
+			string_builder_append_single(string_builder, "permessage-deflate");
+
+			WsDeflateOptions options = extension.data.deflate;
+
+			// always return our window sizes
+			size_t additional_options_count = 0;
+
+			if(options.client.no_context_takeover) {
+				additional_options_count++;
+			}
+
+			if(options.server.no_context_takeover) {
+				additional_options_count++;
+			}
+
+			{
+
+				string_builder_append_single(string_builder, "server_max_window_bits=");
+
+				STRING_BUILDER_APPENDF(string_builder, return false;
+				                       , "%d", options.server.max_window_bits);
+
+				string_builder_append_single(string_builder, ";");
+			}
+
+			size_t index = 0;
+			{
+
+				string_builder_append_single(string_builder, "client_max_window_bits=");
+
+				STRING_BUILDER_APPENDF(string_builder, return false;
+				                       , "%d", options.client.max_window_bits);
+
+				if(additional_options_count != index) {
+					string_builder_append_single(string_builder, ";");
+				}
+
+				++index;
+			}
+
+			if(options.server.no_context_takeover) {
+
+				string_builder_append_single(string_builder, "server_no_context_takeover");
+
+				if(additional_options_count != index) {
+					string_builder_append_single(string_builder, ";");
+				}
+
+				++index;
+			}
+
+			if(options.client.no_context_takeover) {
+
+				string_builder_append_single(string_builder, "client_no_context_takeover");
+			}
+
+			break;
+		}
+		default: {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+NODISCARD static char* get_accepted_ws_extensions_as_string(WSExtensions extensions) {
+
+	StringBuilder* string_builder = string_builder_init();
+
+	if(!string_builder) {
+		return NULL;
+	}
+
+	size_t extensions_length = stbds_arrlenu(extensions);
+
+	for(size_t i = 0; i < extensions_length; ++i) {
+		WSExtension extension = extensions[i];
+
+		bool success = append_ws_extension_as_string(string_builder, extension);
+
+		if(!success) {
+			free_string_builder(string_builder);
+			return NULL;
+		}
+
+		if(i != extensions_length - 1) {
+			string_builder_append_single(string_builder, ",");
+		}
+	}
+
+	return string_builder_release_into_string(&string_builder);
+}
+
 static const bool send_http_upgrade_required_status_code = true;
 
 int handle_ws_handshake(const HttpRequest* const http_request,
@@ -349,7 +449,7 @@ int handle_ws_handshake(const HttpRequest* const http_request,
 				return send_failed_handshake_message(
 				    descriptor, "sec-websocket-version has invalid value", send_settings);
 			}
-		} else if(strcasecmp(header.key, "sec-webSocket-extensions") == 0) {
+		} else if(strcasecmp(header.key, "sec-websocket-extensions") == 0) {
 			// TODO(Totto): this header field may be specified multiple times, but we should
 			// combine all and than parse it, but lets see if the autobahn test suite tests for
 			// that first
@@ -397,13 +497,33 @@ int handle_ws_handshake(const HttpRequest* const http_request,
 
 	char* key_answer = generate_key_answer(sec_key);
 
-	char* sec_websocket_accept_header_buffer = NULL;
-	FORMAT_STRING(&sec_websocket_accept_header_buffer, return false;
-	              , "%s%c%s", "Sec-WebSocket-Accept", '\0', key_answer);
+	if(key_answer != NULL) {
 
-	free(key_answer);
+		char* sec_websocket_accept_header_buffer = NULL;
+		FORMAT_STRING(&sec_websocket_accept_header_buffer, return false;
+		              , "%s%c%s", "Sec-WebSocket-Accept", '\0', key_answer);
 
-	add_http_header_field_by_double_str(&additional_headers, sec_websocket_accept_header_buffer);
+		free(key_answer);
+
+		add_http_header_field_by_double_str(&additional_headers,
+		                                    sec_websocket_accept_header_buffer);
+	}
+
+	if(stbds_arrlenu(extensions) > 0) {
+		char* accepted_extensions = get_accepted_ws_extensions_as_string(extensions);
+
+		if(accepted_extensions != NULL) {
+
+			char* sec_websocket_extensions_header_buffer = NULL;
+			FORMAT_STRING(&sec_websocket_extensions_header_buffer, return false;
+			              , "%s%c%s", "Sec-WebSocket-Extensions", '\0', accepted_extensions);
+
+			free(accepted_extensions);
+
+			add_http_header_field_by_double_str(&additional_headers,
+			                                    sec_websocket_extensions_header_buffer);
+		}
+	}
 
 	HTTPResponseToSend to_send = { .status = HttpStatusSwitchingProtocols,
 		                           .body = http_response_body_empty(),
