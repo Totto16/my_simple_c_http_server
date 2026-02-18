0b10656eea2f1f68488f70e62167a1009addd42c
Totto16 <tobiausgais@gmail.com>
2026-02-18 15:14:51 +0100

    fix: fix macos size_t formatting again

--- src/ws/handler.c
+++ src/ws/handler.c
@@ -12,15 +12,14 @@ WebSocketAction websocket_function(WebSocketConnection* connection, WebSocketMes
 		if(message->is_text) {
 
 			if(message->buffer.size >= MAX_PRINT_FOR_TEXT_MESSAGE) {
-				LOG_MESSAGE(LogLevelInfo, "Received TEXT message of length %" PRIu64 "\n",
+				LOG_MESSAGE(LogLevelInfo, "Received TEXT message of length %zu\n",
 				            message->buffer.size);
 			} else {
 				LOG_MESSAGE(LogLevelInfo, "Received TEXT message: '%.*s'\n",
 				            (int)(message->buffer.size), (char*)message->buffer.data);
 			}
 		} else {
-			LOG_MESSAGE(LogLevelInfo, "Received BIN message of length %" PRIu64 "\n",
-			            message->buffer.size);
+			LOG_MESSAGE(LogLevelInfo, "Received BIN message of length %zu\n", message->buffer.size);
 		}
 	}
 
