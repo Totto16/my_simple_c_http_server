8c152053cf5efc4de69c57e7a318bf68efc61dd0
Totto16 <tobiausgais@gmail.com>
2026-03-27 14:12:15 +0100

    chore: fix pnpm-lock.yaml dependency - link real pnpm-lock.yaml to a file in the build dir, so that meson or ninja checks the modification of that file and considers it as a dependency of the helper script and also runs pnpm install before using anything that relies on it

--- tools/helper/meson_wrapper.sh
+++ tools/helper/meson_wrapper.sh
@@ -4,11 +4,15 @@ set -eu
 
 TO_EXECUTE=()
 
-WRAPPER_GENERATE="false"
+WRAPPER_GENERATE="none"
 WRAPPER_GENERATE_CONTENT=""
 
+LINK_FILE="none"
+LINK_FILE_INPUT=""
+LINK_FILE_OUTPUT=""
+
 for ITEM in "$@"; do
-    if [ "$WRAPPER_GENERATE" == "true" ]; then
+    if [ "$WRAPPER_GENERATE" == "parsing" ]; then
 
         if [ -n "$WRAPPER_GENERATE_CONTENT" ]; then
             echo "ERROR: wrapper generate content passed twice" >&2
@@ -16,30 +20,53 @@ for ITEM in "$@"; do
         fi
 
         WRAPPER_GENERATE_CONTENT="$ITEM"
-        WRAPPER_GENERATE="false"
+        WRAPPER_GENERATE="given"
+    elif [ "$LINK_FILE" == "parsing" ]; then
+
+        if [ -n "$LINK_FILE_OUTPUT" ]; then
+            echo "ERROR: link file argument passed twice" >&2
+            exit 1
+        fi
+
+        if [ -n "$LINK_FILE_INPUT" ]; then
+            LINK_FILE_OUTPUT="$ITEM"
+            LINK_FILE="given"
+        else
+            LINK_FILE_INPUT="$ITEM"
+            LINK_FILE="parsing"
+        fi
+
     else
 
         if [ "$ITEM" == "--wrapper-generate" ]; then
 
-            if [ "$WRAPPER_GENERATE" == "true" ]; then
+            if [ "$WRAPPER_GENERATE" != "none" ]; then
                 echo "ERROR: '--wrapper-generate' passed twice" >&2
                 exit 1
             fi
 
-            WRAPPER_GENERATE="true"
+            WRAPPER_GENERATE="parsing"
+        elif [ "$ITEM" == "--link-file" ]; then
+
+            if [ "$LINK_FILE" != "none" ]; then
+                echo "ERROR: '--link-file' passed twice" >&2
+                exit 1
+            fi
+
+            LINK_FILE="parsing"
         else
             TO_EXECUTE+=("$ITEM")
         fi
     fi
 done
 
-if [ "$WRAPPER_GENERATE" == "true" ]; then
+if [ "$WRAPPER_GENERATE" != "given" ]; then
     echo "ERROR: '--wrapper-generate' missing argument" >&2
     exit 1
 fi
 
 if [ -z "$WRAPPER_GENERATE_CONTENT" ]; then
-    echo "ERROR: not wrapper generate argument passed" >&2
+    echo "ERROR: no wrapper generate argument passed" >&2
     exit 1
 fi
 
@@ -47,3 +74,18 @@ date -Iseconds >"$WRAPPER_GENERATE_CONTENT"
 
 ## execute
 "${TO_EXECUTE[@]}"
+
+if [ "$LINK_FILE" == "given" ]; then
+
+    if [ -z "$LINK_FILE_INPUT" ]; then
+        echo "ERROR: no link file argument passed" >&2
+        exit 1
+    fi
+
+    if [ -z "$LINK_FILE_OUTPUT" ]; then
+        echo "ERROR: no link file argument passed" >&2
+        exit 1
+    fi
+
+    ln -sv "$(realpath "$LINK_FILE_INPUT")" "$(realpath "$LINK_FILE_OUTPUT")"
+fi
