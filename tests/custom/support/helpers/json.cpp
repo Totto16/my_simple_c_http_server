8b86e2ce13a27fb843a299655795ca0d3770d160
Totto16 <tobiausgais@gmail.com>
2026-04-04 16:25:35 +0200

    feat: implement more helpers for json types - make helper functions for the opaque types - also make cpp helper classes, to handle operations in a c++ idiomatic way

--- tests/custom/support/helpers/json.cpp
+++ tests/custom/support/helpers/json.cpp
@@ -1,23 +1,6 @@
-
-
 #include "./json.hpp"
 
 #include "./cpp_types.hpp"
-
-[[nodiscard]] static bool operator==(const JsonBoolean& json_boolean1,
-                                     const JsonBoolean& json_boolean2) {
-	return json_boolean1.value == json_boolean2.value;
-}
-
-[[nodiscard]] static bool operator==(const JsonNumber& json_number1,
-                                     const JsonNumber& json_number2) {
-	return json_number1.value == json_number2.value;
-}
-
-[[nodiscard]] static bool operator==(const JsonString* const json_string1,
-                                     const JsonString* const json_string2) {
-	return json_string_eq(json_string1, json_string2);
-}
 
 [[nodiscard]] bool operator==(const JsonVariant& json_variant1, const JsonVariant& json_variant2) {
 	const auto tag1 = get_current_tag_type_for_json_variant(json_variant1);
@@ -61,7 +44,27 @@
 
 			IF_JSON_VARIANT_IS_STRING_CONST(json_variant2, string_2) {
 
-				return string_1 == string_2;
+				return JsonStringCpp{ string_1 } == string_2;
+			}
+
+			return false;
+		}
+		VARIANT_CASE_END();
+		CASE_JSON_VARIANT_IS_ARRAY_CONST(json_variant1, array_1) {
+
+			IF_JSON_VARIANT_IS_ARRAY_CONST(json_variant2, array_2) {
+
+				return JsonArrayCpp{ array_1.arr } == array_2.arr;
+			}
+
+			return false;
+		}
+		VARIANT_CASE_END();
+		CASE_JSON_VARIANT_IS_OBJECT_CONST(json_variant1, object_1) {
+
+			IF_JSON_VARIANT_IS_OBJECT_CONST(json_variant2, object_2) {
+
+				return JsonObjectCpp{ object_1.obj } == object_2.obj;
 			}
 
 			return false;
@@ -80,4 +83,70 @@ std::ostream& operator<<(std::ostream& os, const JsonVariant& json_variant) {
 	os << str;
 
 	return os;
+}
+
+[[nodiscard]] bool operator==(const JsonBoolean& json_boolean1, const JsonBoolean& json_boolean2) {
+	return json_boolean1.value == json_boolean2.value;
+}
+
+[[nodiscard]] bool operator==(const JsonNumber& json_number1, const JsonNumber& json_number2) {
+	return json_number1.value == json_number2.value;
+}
+
+JsonStringCpp::JsonStringCpp(JsonString* value) : m_value{ value } {}
+
+[[nodiscard]] bool JsonStringCpp::operator==(const JsonStringCpp& json_string2) const {
+	return json_string_eq(this->m_value, json_string2.m_value);
+}
+
+[[nodiscard]] bool JsonStringCpp::operator==(const JsonString* const json_string2) const {
+	return json_string_eq(this->m_value, json_string2);
+}
+
+JsonArrayCpp::JsonArrayCpp(JsonArray* value) : m_value{ value } {}
+
+[[nodiscard]] static bool json_array_eq_impl(const JsonArray* const json_array1,
+                                             const JsonArray* const json_array2) {
+	const size_t size1 = json_array_size(json_array1);
+	const size_t size2 = json_array_size(json_array2);
+
+	if(size1 != size2) {
+		return false;
+	}
+
+	for(size_t i = 0; i < size1; ++i) {
+
+		const auto val1 = json_array_at(json_array1, i);
+		const auto val2 = json_array_at(json_array2, i);
+
+		if(val1 != val2) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+[[nodiscard]] bool JsonArrayCpp::operator==(const JsonArrayCpp& json_array2) const {
+	return json_array_eq_impl(this->m_value, json_array2.m_value);
+}
+
+[[nodiscard]] bool JsonArrayCpp::operator==(const JsonArray* const json_array2) const {
+	return json_array_eq_impl(this->m_value, json_array2);
+}
+
+
+JsonObjectCpp::JsonObjectCpp(JsonObject* value) : m_value{ value } {}
+
+[[nodiscard]] static bool json_object_eq_impl(const JsonObject* const json_object1,
+                                             const JsonObject* const json_object2) {
+	
+}
+
+[[nodiscard]] bool JsonObjectCpp::operator==(const JsonObjectCpp& json_object2) const {
+	return json_object_eq_impl(this->m_value, json_object2.m_value);
+}
+
+[[nodiscard]] bool JsonObjectCpp::operator==(const JsonArray* const json_object2) const {
+	return json_object_eq_impl(this->m_value, json_object2);
 }
--- tests/custom/support/helpers/json.hpp
+++ tests/custom/support/helpers/json.hpp
@@ -5,7 +5,73 @@
 #include <json/json.h>
 
 #include <ostream>
+#include <ranges>
 
 [[nodiscard]] bool operator==(const JsonVariant& json_variant1, const JsonVariant& json_variant2);
 
 std::ostream& operator<<(std::ostream& os, const JsonVariant& json_variant);
+
+[[nodiscard]] bool operator==(const JsonBoolean& json_boolean1, const JsonBoolean& json_boolean2);
+
+[[nodiscard]] bool operator==(const JsonNumber& json_number1, const JsonNumber& json_number2);
+
+struct JsonStringCpp {
+  private:
+	JsonString* m_value;
+
+  public:
+	explicit JsonStringCpp(JsonString* value);
+
+	[[nodiscard]] bool operator==(const JsonStringCpp& json_string2) const;
+
+	[[nodiscard]] bool operator==(const JsonString* json_string2) const;
+};
+
+struct JsonArrayCpp {
+  private:
+	JsonArray* m_value;
+
+  public:
+	explicit JsonArrayCpp(JsonArray* value);
+
+	[[nodiscard]] bool operator==(const JsonArrayCpp& json_array2) const;
+
+	[[nodiscard]] bool operator==(const JsonArray* json_array2) const;
+
+	[[nodiscard]] JsonVariant& operator[](size_t index);
+
+	[[nodiscard]] const JsonVariant& operator[](size_t index) const;
+
+	[[nodiscard]] size_t size() const;
+
+	[[nodiscard]] JsonVariant* begin();
+	[[nodiscard]] JsonVariant* end();
+
+	[[nodiscard]] const JsonVariant* begin() const;
+
+	[[nodiscard]] const JsonVariant* end() const;
+};
+
+static_assert(std::ranges::range<JsonArrayCpp>);
+
+struct JsonObjectEntryCpp {
+	int todo;
+};
+
+struct JsonObjectCpp {
+  private:
+	JsonObject* m_value;
+
+  public:
+	explicit JsonObjectCpp(JsonObject* value);
+
+	[[nodiscard]] bool operator==(const JsonObjectCpp& json_object2) const;
+
+	[[nodiscard]] bool operator==(const JsonObject* json_object2) const;
+
+	[[nodiscard]] const JsonObjectEntryCpp* begin() const;
+
+	[[nodiscard]] const JsonObjectEntryCpp* end() const;
+};
+
+static_assert(std::ranges::range<JsonObjectCpp>);
